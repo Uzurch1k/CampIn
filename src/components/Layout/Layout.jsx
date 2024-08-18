070f5bd8d93ce93326b310b9477a50ce511df0ae
Uzurch1k <galcenkovlad7@gmail.com>
2024-08-19 00:24:21 +0300

    fix

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,14 @@
 import { Suspense } from 'react';
 
 import Navigation from '../Navigation/Navigation';
+import { LoaderMain } from '../Loader/Loader';
 
 const Layout = ({ children }) => {
   return (
     <div className="wrapper">
       <Navigation />
       <main className="main">
-        <Suspense>{children}</Suspense>
+        <Suspense fallback={<LoaderMain />}>{children}</Suspense>
       </main>
     </div>
   );
